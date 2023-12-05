2dc88c8f15ab8060dab0d490b38b3d2b1ff66e1e
hafsachenni <hafsachenni77@gmail.com>
2023-12-05 12:12:37 -0800

    its done

--- models/base_model.py
+++ models/base_model.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
 
 import uuid
-import datetime
+from datetime import datetime
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
-        self.created_at = None
-        self.updated_at = None
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-        def __str__(self):
-            return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
-        def save(self):
-            self.updated_at = datetime.now()
+    def save(self):
+        self.updated_at = datetime.now()
 
-        def to_dict(self):
-            obj = self.__dict__.copy()
-            obj['__class__'] = self.__class__.name__
-            obj['created_at'] = self.created_at.isoformat()
-            obj['updated_at'] = self.updated_at.isoformat()
-            return obj
-
-
+    def to_dict(self):
+        obj = self.__dict__.copy()
+        obj['__class__'] = self.__class__.__name__
+        obj['created_at'] = self.created_at.isoformat()
+        obj['updated_at'] = self.updated_at.isoformat()
+        return obj
