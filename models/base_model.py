21ad7548b444decfa388d62f5f750854e4fc13a9
hafsachenni <hafsachenni77@gmail.com>
2023-12-05 10:01:45 -0800

    isoformat

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,20 @@ import datetime
 class BaseModel:
     def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = None
+        self.updated_at = None
 
         def __str__(self):
             return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+        def save(self):
+            self.updated_at = datetime.now()
+
+        def to_dict(self):
+            obj = self.__dict__.copy()
+            obj['__class__'] = self.__class__.name__
+            obj['created_at'] = self.created_at.isoforma()
+            obj['updated_at'] = self.updated_at.isoformat()
+            return obj
 
 
