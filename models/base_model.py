9258046d0077dd3abf0ef437d0a3d376b652e837
fatimazaarate <fatima.zaarate@gmail.com>
2023-12-05 20:31:15 +0100

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 import uuid
-import datetime
+from datetime import datetime
 
 class BaseModel:
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
@@ -18,8 +18,19 @@ class BaseModel:
     def to_dict(self):
         obj = self.__dict__.copy()
         obj['__class__'] = self.__class__.__name__
-        return self.__dict__
+        obj['created_at'] = self.created_at.isoformat()
+        obj['updated_at'] = self.updated_at.isoformat()
+        return obj
 
 
-    def get_id(self):
-        return self.id
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
