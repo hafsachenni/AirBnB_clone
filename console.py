8d14d42f10509938ddf54fc2c792ce5cb8a0aaf1
fatimazaarate <fatima.zaarate@gmail.com>
2023-12-06 22:56:28 +0100

    task 6 done

--- console.py
+++ console.py
@@ -3,17 +3,20 @@
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    """ entry point of the command interpreter"""
-    prompt = "(hnbn)"
+    """Entry point of the command interpreter"""
+    prompt = "(hnbn) "
 
     def do_quit(self, arg):
-        """quit command to exit program"""
+        """Quit command to exit program\n"""
         return True
 
     def do_EOF(self, arg):
-        """eof command to exit program"""
-        print()
+        """EOF command to exit program\n"""
         return True
+
+    def emptyline(self):
+        """Doesn't execute anything"""
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
