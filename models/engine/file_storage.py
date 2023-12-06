b709b9b28a017cb029dfbfb1e39360e0a61e04bd
hafsachenni <hafsachenni77@gmail.com>
2023-12-06 04:22:43 -0800

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,3 +12,15 @@ class FileStorage:
     def all(self):
         return self.__objects
 
+    def new(self, obj):
+        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+
+    def save(self):
+        objects = {key: obj.to_dict() for key, obj in self.__objects__.items()
+        with open (__file_path, "w", encoding='utf-8') as file: 
+            json.dump(objects, file)
+
+
+    def relaod(self):
+        with open(__file__path, "r", encoding='utf-8') as file:
+            json.load(objects, file)
