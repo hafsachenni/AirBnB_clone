5d30a5e0704d21e01a451e220607d289f62c4977
hafsachenni <hafsachenni77@gmail.com>
2023-12-06 07:58:36 -0800

    reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,28 +5,29 @@ from models.base_model import BaseModel
 
 class FileStorage:
 
-    def __init__(self, file_path="file.json", objects=None):
-        self.__file_path = file.json
-        self.__objects = {}
+        __file_path = "file.json"
+        __objects = {}
 
-    def all(self):
-        return self.__objects
+        def all(self):
+            return self.__objects
 
-    def new(self, obj):
-        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+        def new(self, obj):
+            self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
 
-    def save(self):
-        objects = {key: obj.to_dict() for key, obj in self.__objects.items()}
-        with open(self.__file_path, "w", encoding='utf-8') as file: 
-            json.dump(objects, file)
+        def save(self):
+            objects = {key: obj.to_dict() for key, obj in self.__objects.items()}
+            with open(self.__file_path, "w", encoding='utf-8') as file: 
+                json.dump(objects, file)
 
 
-    def reload(self):
-        try:
-            with open(self.__file_path, "r", encoding='utf-8') as file:
-                loaded_objects = json.load(file)
-        except FileNotFoundError:
-            pass
+        def reload(self):
+            try:
+                with open(self.__file_path, "r", encoding='utf-8') as file:
+                    loaded_objects = json.load(file)
+                    for key, obj_data in loaded_objects.items():
+                        self.__objects[key] = eval(obj["__class__"])(**obj)
+            except FileNotFoundError:
+                pass
 
 
 all_objs = storage.all()
