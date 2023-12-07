411814c26744bfbdd4df8030acda4a4c7a55a42c
hafsachenni <hafsachenni77@gmail.com>
2023-12-07 05:12:32 -0800

    no instance found

--- console.py
+++ console.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """ entry point of the command interpreter"""
     prompt = "(hnbn) "
+    dict_classes = {
+            "BaseModel": BaseModel
+            }
+
 
     def do_quit(self, arg):
         """Quit command to exit program\n"""
@@ -18,6 +24,43 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """does nothing"""
         pass
+
+
+    def do_create(self, args):
+        """creates a new instance of the given class"""
+        args = args.split()
+        if not args:
+            print("**class name missing**")
+            return
+        if args[0] not in HBNBCommand.dict_classes:
+            print("** class doesn't exist **")
+            return
+
+        new_object = HBNBCommand.dict_classes[args]()
+        storage.save()
+        print(new_object.id)
+
+
+    def do_show(self, args):
+        """prints str representation of an obj"""
+        args = args.split()
+        if not args:
+            print("** class name missing **")
+            return
+        if args[0] not in HBNBCommand.dict_classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+
+        str_repre = "{}.{}".format(args[0], args[1])
+        if str_repre not in storage.all():
+            print("** no instance found **")
+            return
+
+        print(storage.all()[str_repre])
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
