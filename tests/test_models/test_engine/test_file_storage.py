b200f3a0a855ef937d14170e73066f175c15e7ac
hafsachenni <hafsachenni77@gmail.com>
2023-12-08 08:24:55 -0800

    engine unittest done

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+unittest for filestorage module
+"""
+
+import unittest
+from models.engine.file_storage import FileStorage
+import models
+from models.user import User
+from models.base_model import BaseModel
+
+
+class TestStorage(unittest.TestCase):
+    """
+    testing the filestorage class
+    """
+
+    def test_instance(self):
+        self.assertIs(FileStorage, type(FileStorage()))
+        self.assertIsInstance(FileStorage._FileStorage__objects, dict)
+        self.assertIsInstance(FileStorage._FileStorage__file_path, str)
+
+
+    def test_all_in_storage(self):
+        self.assertEqual(type(models.storage.all()), dict) 
+
+
+    def test_FileStorage(self):
+        self.assertEqual(type(models.storage), FileStorage)
